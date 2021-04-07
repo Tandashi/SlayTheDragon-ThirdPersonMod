9b55bb7d000fa1603232f38354679a799d58f341
Tandashi <tandashi@tandashi.de>
2021-04-07 20:12:53 +0200

    Working v1.0.0

---
+++ ThirdPerson Mod/AddCamerasToManagerPatch.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable CS0626
+
+using MonoMod;
+using Modding.API;
+
+[MonoModPatch("global::AddCamerasToManager")]
+class AddCamerasToManagerPatch : AddCamerasToManager {
+
+    public extern void orig_Start();
+    public void Start() {
+        orig_Start();
+
+        ThirdPersonAPI.ActivateThirdPerson();
+    }
+
+}
